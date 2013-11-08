996cddd9db159fd30f6fd2b08cfb74adde6ca40e
Holger Friedrich <holger.friedrich@bekk.no>
2013-11-08 15:02:37 +0100

    PostDeploy-script deletes transformasjonsfiler

--- MvcApplication/PostDeploy.ps1
+++ MvcApplication/PostDeploy.ps1
@@ -1,5 +1,15 @@
-﻿$imagepath = $OctopusParameters["Imagepath"]
-
-if (Test-Path $imagepath){
-	Copy-Item $imagepath .\Images\bilde.jpg
+﻿
+function cleanUpConfigFiles(){
+	Get-ChildItem -recurse .\ | ? { $_.Name -match '^\w+\.\w+\.config'}
 }
+
+function replaceImage(){
+	$imagepath = $OctopusParameters["Imagepath"]
+
+	if (Test-Path $imagepath){
+		Copy-Item $imagepath .\Images\bilde.jpg
+	}
+}
+
+cleanUpConfigFiles
+replaceImage
